8addf66e8aa061a011094a51e9c815b40658741b
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-07-09 20:40:07 +0300

    Final Initial Commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,7 @@
 const app = require('express')();
 
+app.get('/', (req, res) => {
+  res.json({ commit: 'Inital Commit' });
+});
+
 app.listen(3001, () => console.log('Server listening on port 3001...'));
